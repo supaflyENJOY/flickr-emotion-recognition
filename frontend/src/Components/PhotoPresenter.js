a3431af2d67c8454334d126f59ead0ed30b87d5d
Gastonovich <oleksndrkorn@gmail.com>
2019-02-09 12:05:35 +0200

    change naming and add stateless component for mapping photos

--- frontend/src/Components/PhotoPresenter.js
+++ frontend/src/Components/PhotoPresenter.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import PopUp from './PopUp';
+import Photos from './Photos';
 
 const PhotosWrapper = styled.div`
   margin-top: 100px;
@@ -19,81 +20,31 @@ const PhotosWrapper = styled.div`
     height:360px;
   }
 `;
-const Layout = styled.div`
-  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.16);
-  background-color: #ffffff;
-`;
-const ImageWrapper = styled.div`
-  display: flex;
-  
-  width 90%;
-  margin-left:5%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  >img{
-    margin-top: 18px;
-    width:370px;
-    height:260px;
-  }
-  text-align:left;
-`;
 
-const Description = styled.div`
-  width: 100%;
-  color: #333;
-  h4 {
-    margin: 20px 0 8px 0;
-    font-family: Muller;
-    font-size: 21px;
-    font-weight: 500;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.19;
-    letter-spacing: normal;
-  }
-  p {
-    margin: 0;
-    font-family: Muller;
-    font-size: 17px;
-    color: #959595;
-  }
-`;
-
-export default function ImagesContainer({ data }) {
-  const [id, setID] = useState(null);
+export default function PhotosPresenter({ data }) {
+  const [id, setId] = useState(null);
   function nextPhoto() {
     if (id < data.length - 1) {
-      setID(id + 1);
+      setId(id + 1);
     }
   }
   function previousPhoto() {
     if (id >= 1) {
-      setID(id - 1);
+      setId(id - 1);
     }
   }
   function clear() {
-    setID(null);
+    setId(null);
   }
-
-  const photos = data.map(photo => (
-    <Layout key={photo.id} onClick={() => setID(photo.id)}>
-      <ImageWrapper>
-        <img src={require('../images/image.png')} alt={'faces'} />
-        <Description>
-          <h4>{photo.id}</h4>
-          <p>{photo.text}</p>
-        </Description>
-      </ImageWrapper>
-    </Layout>
-  ));
 
   return (
     <>
       {id !== null ? (
         <PopUp photo={data[id]} back={previousPhoto} next={nextPhoto} exit={clear} />
       ) : null}
-      <PhotosWrapper>{photos}</PhotosWrapper>
+      <PhotosWrapper>
+        <Photos data={data} changeId={setId} />
+      </PhotosWrapper>
     </>
   );
 }
---
+++ frontend/src/Components/Photos.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  
+  width 90%;
+  margin-left:5%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  >img{
+    margin-top: 18px;
+    width:370px;
+    height:260px;
+  }
+  text-align:left;
+`;
+
+const Description = styled.div`
+  width: 100%;
+  color: #333;
+  h4 {
+    margin: 20px 0 8px 0;
+    font-family: Muller;
+    font-size: 21px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.19;
+    letter-spacing: normal;
+  }
+  p {
+    margin: 0;
+    font-family: Muller;
+    font-size: 17px;
+    color: #959595;
+  }
+`;
+
+export default function photos({ data, changeId }) {
+  return data.map(photo => (
+    <Layout key={photo.id} onClick={() => changeId(photo.id)}>
+      <ImageWrapper>
+        <img src={require('../images/image.png')} alt={'faces'} />
+        <Description>
+          <h4>{photo.id}</h4>
+          <p>{photo.text}</p>
+        </Description>
+      </ImageWrapper>
+    </Layout>
+  ));
+}
